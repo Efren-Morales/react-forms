71e17bb013c110ab130a053595d7d2d47f2f38ed
Efren Morales <efrenmorales@Efrens-MacBook-Pro.local>
2019-04-02 10:17:58 -0600

    removed passwordreset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './Login.js';
-import Passwordreset from './Passwordreset.js';
+
 
 class App extends Component {
   render() {
     return (
       <Login></Login>,
-      <Passwordreset></Passwordreset> 
+    
     );
   }
 }
